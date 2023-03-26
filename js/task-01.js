a56b45fa6739c2763d2bea9c0238458e1d586018
SzymczakJakub <116820464+SzymczakJakub@users.noreply.github.com>
2023-03-26 08:59:30 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const categoriesList = document.querySelector("#categories");
+const categoriesItems = categoriesList.querySelectorAll("li.item");
+console.log(`Liczba kategorii: ${categoriesItems.length}`);
+
+categoriesItems.forEach((item) => {
+  const categoryName = item.querySelector("h2").textContent;
+  const categoryElement = item.querySelectorAll("li");
+
+  console.log(`Category: ${categoryName}, Elements: ${categoryElement.length}`);
+});
