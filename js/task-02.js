2d047f4733a7b88dd41560953319a98c358282ff
SzymczakJakub <116820464+SzymczakJakub@users.noreply.github.com>
2023-03-26 09:28:03 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
+const list = document.querySelector("#ingredients");
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+ingredients.forEach((ingredient) => {
+  let element = document.createElement("li");
+  element.textContent = ingredient;
+  element.classList.add("item");
+  list.appendChild(element);
+});
