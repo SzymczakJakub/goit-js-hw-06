1fe2cb041e0a30204c8de8a23848f54437d56324
SzymczakJakub <116820464+SzymczakJakub@users.noreply.github.com>
2023-03-26 19:11:39 +0200

    7,8,9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = loginForm.elements.email.value;
+  const password = loginForm.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("Wypełnij wszystkie pola!");
+  } else {
+    const formData = { email, password };
+    console.log(formData);
+    loginForm.reset();
+  }
+});
